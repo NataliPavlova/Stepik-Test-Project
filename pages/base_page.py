4a9c05e4176f3fb3677c9dc5bd27a5e6cb07adb4
Natali <NataliRomPav@gmail.com>
2021-11-26 09:49:07 +0300

    Параметризация теста в файле test_product_page.py (links)

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,8 @@
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 import math
 
 class BasePage():
@@ -20,6 +23,7 @@ class BasePage():
         return True
 
     def solve_quiz_and_get_code(self):
+        WebDriverWait(self.browser, 10).until(EC.alert_is_present())
         alert = self.browser.switch_to.alert
         x = alert.text.split(" ")[2]
         answer = str(math.log(abs((12 * math.sin(float(x))))))
--- pages/product_page.py
+++ pages/product_page.py
@@ -10,13 +10,13 @@ class ProductPage(BasePage):
 
     def should_be_correct_message_product_in_basket(self):
         # проверяем совпадает ли название товара и цена в сообщении (в корзине) с тем,что мы добавили
-        name = self.product_name()
-        name_in_basket = self.message_product_name_in_basket()
-        self.should_be_correct_message_product_name_in_basket(name, name_in_basket)
+        product_name = self.product_name()
+        product_name_in_basket = self.message_product_name_in_basket()
+        self.should_be_correct_message_product_name_in_basket(product_name, product_name_in_basket)
 
-        price = self.product_price()
-        price_in_basket = self.message_product_price_in_basket()
-        self.should_be_correct_message_product_price_in_basket(price, price_in_basket)
+        product_price = self.product_price()
+        product_price_in_basket = self.message_product_price_in_basket()
+        self.should_be_correct_message_product_price_in_basket(product_price, product_price_in_basket)
 
     def product_name(self):
         #  фиксируем (возвращаем) название товара  из карточки товара, добавляемого в корзину
@@ -30,7 +30,7 @@ class ProductPage(BasePage):
         product_name_in_basket = product_name_element_in_basket.text
         return product_name_in_basket
 
-    def should_be_correct_message_product_name_in_basket(self, product_name, product_name_in_basket ):
+    def should_be_correct_message_product_name_in_basket(self, product_name, product_name_in_basket):
         # проверяем совпадают ли названия товара, который добавили в корзину с названием в сообщении
         # для проверки наименование товара передаютя в качестве параметров
         # product_name = self.product_name()
@@ -39,16 +39,16 @@ class ProductPage(BasePage):
 
     # аналогично и с ценой
     def product_price(self):
-        product_price_element = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_ELEMENT)
-        product_price = product_price_element.text
+        product_price_el = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_ELEMENT)
+        product_price = product_price_el.text
         return product_price
 
     def message_product_price_in_basket(self):
-        product_price_element_in_basket = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_MESSAGE_IN_BASKET)
-        product_price_in_basket = product_price_element_in_basket.text
+        product_price_el_in_basket = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_MESSAGE_IN_BASKET)
+        product_price_in_basket = product_price_el_in_basket.text
         return product_price_in_basket
 
-    def should_be_correct_message_product_price_in_basket(self, product_price,product_price_in_basket):
+    def should_be_correct_message_product_price_in_basket(self, product_price, product_price_in_basket):
         # product_price = self.product_price()
         # product_price_in_basket = self.message_product_price_in_basket()
         assert product_price == product_price_in_basket, 'Non'
